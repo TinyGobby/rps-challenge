ac70e869f080538e2a64a4655291787384a38840
Patrick Harris <patricknpharris@gmail.com>
2018-09-22 11:44:43 +0200

    Additional tests added for paper and scissors choices

--- spec/features/choices_spec.rb
+++ spec/features/choices_spec.rb
@@ -3,11 +3,25 @@ feature 'choices' do
     sign_in_and_play
     expect(page).to have_content('Rock Paper Scissors')
   end
-  
+
   scenario 'after choosing "Rock" see confirmation' do
     sign_in_and_play
     choose('Rock')
     click_button('Submit')
     expect(page).to have_content('You chose Rock')
+  end
+
+  scenario 'after choosing "Paper" see confirmation' do
+    sign_in_and_play
+    choose('Paper')
+    click_button('Submit')
+    expect(page).to have_content('You chose Paper')
+  end
+
+  scenario 'after choosing "Scissors" see confirmation' do
+    sign_in_and_play
+    choose('Scissors')
+    click_button('Submit')
+    expect(page).to have_content('You chose Scissors')
   end
 end
